f844738c88eef0adac4bd7f8d546142c2a08dc0e
Adrienne Kecskes <79340489+akecs512@users.noreply.github.com>
2023-10-05 22:11:05 -0500

    Update Home.jsx conflicts resolved
    resolved conflicts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,25 +1,17 @@
 import { useQuery } from "@apollo/client";
 
-<<<<<<< HEAD
 
 import ActivityForm from '../components/ActivityForm';
 
 import { QUERY_ACTIVITIES } from '../utils/queries';
-=======
-import UserList from "../components/UserList";
-import UserForm from "../components/UserForm";
 
-import { QUERY_PROFILES } from "../utils/queries";
->>>>>>> 6dda95c53939c19ac973efe39d2142c02484e734
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_ACTIVITIES);
 
-<<<<<<< HEAD
+
   const activities = data?.activities || [];
-=======
-  const users = data?.users || [];
->>>>>>> 6dda95c53939c19ac973efe39d2142c02484e734
+
 
   return (
     <main>
@@ -28,23 +20,12 @@ const Home = () => {
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: "1px dotted #1a1a1a" }}
         >
-<<<<<<< HEAD
+
           <ActivityForm />
         </div>
 
 
-=======
-          <UserForm />
-        </div>
 
-        <div className="col-12 col-md-10 my-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <UserList users={users} title="User List" />
-          )}
-        </div>
->>>>>>> 6dda95c53939c19ac973efe39d2142c02484e734
       </div>
     </main>
   );
