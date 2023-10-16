5ff852821d9dfa1484eec4b1ff850f205405cfd1
Marialdinha <marialda@bellsouth.net>
2023-10-16 18:54:36 -0500

    Modified  workout edit page

--- client/src/pages/EditWorkout.jsx
+++ client/src/pages/EditWorkout.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from 'react';
 import { useLocation  } from 'react-router-dom'
+import { QUERY_WORKOUT } from "../utils/queries";
+import {REMOVE_WORKOUT} from "../utils/mutations"
+import { useQuery , useMutation } from "@apollo/client";
 
 const handleRemoveWorkout = async (workout) => {
   try {
@@ -24,18 +26,16 @@ const handleUpdateWorkout = async (workOutId) => {
 const EditWorkout = () => {
 const location = useLocation()
 const { from } = location.state
-let [workoutObj, setworkoutObj] = useState({})
-console.log(location)
-useEffect(()=>{
-  const workOutId = window.location.pathname.split('/')[2]
-{/* const { loading, data } = useQuery(workout(workOutId));*/}
-  {/* const workout = new workout(workOutId);*/}
-},[ ])
 
-console.log(workoutObj)
+
+const workOutId = window.location.pathname.split('/')[2]
+const { loading, data } = useQuery(QUERY_WORKOUT,{variables:{workoutId: workOutId}});
+const workoutData = data?.workout
+
 
   return (
     <>
+   
       <div
         className="hero min-h-screen"
         style={{ backgroundImage: "url(/musclebg.jpeg" }}
@@ -43,25 +43,30 @@ console.log(workoutObj)
       <div className="card w-96 glass mt-8">
         <div className="card-body">
           <h2 className="card-title justify-center">Edit or Delete Your Workout</h2>
+
+          {loading ? (<div> Loading ...</div>) : (
+      <div>
+         <p>Category - {workoutData.category}</p> 
+         <p>Title - {workoutData.workoutTitle}</p> 
+         <p>Date - {workoutData.workoutDate}</p> 
+         <p>Duration - {workoutData.workoutDuration} minutes</p> 
+         <p>Notes - {workoutData.comment}</p>  
+      </div>)
+    }
+    <div>
           <button
             className="btn btn-sm btn-danger ml-auto"
-            onClick={() => handleRemoveWorkout(workout)}
-                    >
-                      Delete
+            onClick={() => handleRemoveWorkout(workout)}>
+            Delete
           </button>
 
           <button
             className="btn btn-sm btn-danger ml-auto"
-            onClick={() => handleUpdateWorkout(workout)}
-                    >
-                      Edit
+            onClick={() => handleUpdateWorkout(workout)}>
+            Edit
           </button>
-          <p>
-            {/* {workoutObj && <p>{workoutObj.name}</p>} */}
-           {/* {workoutObj && <p>{workoutObj.title}</p>} */}
-          
-          {from.title}
-          </p> 
+
+          </div>
           </div>
         </div>
       </div>
