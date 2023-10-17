708d0464d7e48ece94cad84acb2e7b52e88f828f
Adrienne Kecskes <akecs619@yahoo.com>
2023-10-17 11:45:26 -0500

    refactor workout state to be an object

--- client/src/components/WorkoutForm/index.jsx
+++ client/src/components/WorkoutForm/index.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { useOnClickOutside } from "use-hooks";
 import { workoutCategories } from "../../utils/categories";
@@ -8,14 +8,18 @@ import cn from "classnames";
 import { ADD_WORKOUT } from "../../utils/mutations";
 
 import Auth from "../../utils/auth";
+export const defaultWorkout = {
+  title: "",
+  date: "",
+  duration: 0,
+  comment: "",
+  category: "",
+  userId: Auth.getUser().data._id,
+};
 
-const WorkoutForm = (userId) => {
- 
+const WorkoutForm = ({ userId }) => {
   const [category, setCategory] = useState("");
-  const [workoutTitle, setWorkoutTitle] = useState("");
-  const [workoutDate, setWorkoutDate] = useState("");
-  const [workoutDuration, setWorkoutDuration] = useState(0);
-  const [comment, setComment] = useState("");
+  const [workout, setWorkout] = useState(defaultWorkout);
   const [open, setOpen] = useState();
   const ref = useRef();
   const navigate = useNavigate();
@@ -31,24 +35,20 @@ const WorkoutForm = (userId) => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    const strDuration = workoutDuration.toString();
     try {
       await addWorkout({
         variables: {
           userId: userId,
-          workoutTitle: workoutTitle,
-          workoutDate: workoutDate,
-          workoutDuration: strDuration,
-          comment: comment,
-          category: category
+          workoutTitle: workout.title,
+          workoutDate: workout.date,
+          workoutDuration: workout.duration.toString(),
+          comment: workout.comment,
+          category: category,
         },
       });
 
-      setWorkoutTitle("");
-      setWorkoutDate("");
-      setWorkoutDuration(0);
-      setComment("");
-      setCategory("");
+      setWorkout(defaultWorkout);
+
       navigate("/");
       location.reload();
     } catch (err) {
@@ -70,81 +70,84 @@ const WorkoutForm = (userId) => {
                 onSubmit={handleFormSubmit}
               >
                 <div className="form-control flex flex-col col-12 col-lg-3">
-                <div
-                  className={cn({
-                    "dropdown mb-8": true,
-                    "dropdown-open": open,
-                  })}
-                  ref={ref}
-                >
                   <div
-                    className="m-1 btn"
-                    onClick={() => setOpen((prev) => !prev)}
-                  >
-                    {category || "Select a category"}
-                  </div>
-                  <ul
                     className={cn({
-                      "p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52": true,
-                      hidden: !open,
+                      "dropdown mb-8": true,
+                      "dropdown-open": open,
                     })}
+                    ref={ref}
                   >
-                    {workoutCategories.map((category) => (
-                      <li key={category}>
-                        <a onClick={handleClick}>{category}</a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="form-control flex flex-col col-12 col-lg-9">
-                  <input
-                    placeholder="Name of workout..."
-                    value={workoutTitle}
-                    className="form-input border "
-                    onChange={(event) => setWorkoutTitle(event.target.value)}
-                  />
-
-                  <input
-                    type="date"
-                    placeholder="Date (MM/DD/YYYY)..."
-                    value={workoutDate}
-                    className="form-input "
-                    onChange={(event) => {
-                      // const date = new Date(
-                      //   event.target.value
-                      // ).toLocaleDateString("en-US");
-                      setWorkoutDate(event.target.value);
-                    }}
-                  />
-
-                  <input
-                    type="number"
-                    placeholder="Duration in minutes..."
-                    value={workoutDuration}
-                    className="form-input"
-                    onChange={(event) => setWorkoutDuration(event.target.value)}
-                  />
-                  <textarea
-                    className="form-input"
-                    value={comment}
-                    placeholder="Notes..."
-                    onChange={(event) => setComment(event.target.value)}
-                  ></textarea>
-
-                  <div className="flex col-12 col-lg-3">
-                    <button
-                      className="flex btn btn-info btn-block py-3"
-                      type="submit"
+                    <div
+                      className="m-1 btn"
+                      onClick={() => setOpen((prev) => !prev)}
                     >
-                      Add Workout
-                    </button>
-                  </div>
-                  {error && (
-                    <div className="col-12 my-3 bg-danger text-white p-3">
-                      {error.message}
+                      {category || "Select a category"}
                     </div>
-                  )}
-                </div>
+                    <ul
+                      className={cn({
+                        "p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52": true,
+                        hidden: !open,
+                      })}
+                    >
+                      {workoutCategories.map((category) => (
+                        <li key={category}>
+                          <a onClick={handleClick}>{category}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div className="form-control flex flex-col col-12 col-lg-9">
+                    <input
+                      placeholder="Name of workout..."
+                      value={workout.title}
+                      className="form-input border "
+                      onChange={(event) =>
+                        setWorkout({ ...workout, title: event.target.value })
+                      }
+                    />
+
+                    <input
+                      type="date"
+                      placeholder="Date (MM/DD/YYYY)..."
+                      value={workout.date}
+                      className="form-input "
+                      onChange={(event) => {
+                        setWorkout({ ...workout, date: event.target.value });
+                      }}
+                    />
+
+                    <input
+                      type="number"
+                      placeholder="Duration in minutes..."
+                      value={workout.duration}
+                      className="form-input"
+                      onChange={(event) =>
+                        setWorkout({ ...workout, duration: event.target.value })
+                      }
+                    />
+                    <textarea
+                      className="form-input"
+                      value={workout.comment}
+                      placeholder="Notes..."
+                      onChange={(event) =>
+                        setWorkout({ ...workout, comment: event.target.value })
+                      }
+                    ></textarea>
+
+                    <div className="flex col-12 col-lg-3">
+                      <button
+                        className="flex btn btn-info btn-block py-3"
+                        type="submit"
+                      >
+                        Add Workout
+                      </button>
+                    </div>
+                    {error && (
+                      <div className="col-12 my-3 bg-danger text-white p-3">
+                        {error.message}
+                      </div>
+                    )}
+                  </div>
                 </div>
               </form>
             </div>
